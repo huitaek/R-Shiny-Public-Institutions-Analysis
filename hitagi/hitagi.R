4fccd3a2afdeff888eba6a7c6eff25c2a5913c2c
Hitagi <taeodore@naver.com>
2022-02-24 08:28:25 +0900

    feat, hitagi

--- hitagi/hitagi.R
+++ hitagi/hitagi.R
@@ -40,29 +40,17 @@ ggplot(data, aes(x=avg_sal, y=ser_year)) +
 ggplot(data, aes(x=new_sal, y=ser_year)) + 
   geom_point()
 
-
-
-#install.packages("treemap")
+# 트리맵
 library(treemap)
 treemap(treem<- as.data.frame(table(data$gongtype)),
         index="Var1",
         vSize="Freq",
         type="index"
 )
+
+# 레이더 차트
 install.packages("fmsb")
 library(fmsb)
-
-# Create data: note in High school for Jonathan:
-data <- as.data.frame(matrix( sample( 2:20 , 10 , replace=T) , ncol=10))
-colnames(data) <- c("math" , "english" , "biology" , "music" , "R-coding", "data-viz" , "french" , "physic", "statistic", "sport" )
-
-# To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
-data <- rbind(rep(20,10) , rep(0,10) , data)
-
-# Check your data, it has to look like this!
-# head(data)
-
-# The default radar chart 
 input_year <- 2017
 data1 <- subset(data, (year==input_year)&(institution=='한국과학기술원'))
 radardata <- data1[c("new_sal", "avg_sal", "ser_year", "rate")]
@@ -74,3 +62,25 @@ radardata$평균별점 <- radardata$평균별점*2
 radardata <- rbind(rep(20,10) , rep(0,10) , radardata)
 radarchart(radardata,
            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4)
+
+
+# 연도별로 변화하하는거 대시보드에 넣으면 좋을듯?
+# 그리고 
+# 잡플래닛 점수 분포
+datatemp <- subset(data, year==2017)
+ggplot(data, aes(x=rate)) + 
+  geom_histogram()
+
+# 근속연수 분포
+datatemp <- subset(data, year==2017)
+ggplot(data, aes(x=ser_year/12)) + 
+  geom_histogram()
+
+# 평균연봉 분포 
+datatemp <- subset(data, year==2017)
+ggplot(data, aes(x=avg_sal)) + 
+  geom_histogram() + geom_histogram()
+# 초봉 분포 
+datatemp <- subset(data, year==2017)
+ggplot(data, aes(x=new_sal)) + 
+  geom_histogram()
